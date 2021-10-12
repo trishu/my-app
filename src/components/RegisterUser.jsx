2db6352239a3ed46274525f5672c9ff50c631f36
Trishu <a.trishu@gmail.com>
2021-10-12 12:34:13 +0530

    Added changes for Register User submit

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -1,50 +1,84 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card';
+import { useRef, useState } from 'react';
 
 const RegisterUser = () => {
+
+    let enteredUser = {};
+    const registerUserFName = useRef();
+    const registerUserLName = useRef();
+    const registerUserLocation = useRef();
+    const registerUserPhone = useRef();
+    const registerUserEmail = useRef();
+    const registerUserPassword = useRef();
+    const registerUserConfirmPassword = useRef();
+
+    const [disable, setDisable] = useState(true);
+
+    const matchPassword = () => {
+        const pwd = registerUserPassword.current.value;
+        const confPwd = registerUserConfirmPassword.current.value;
+        if('undefined' !== pwd && 'undefined' !== confPwd && pwd===confPwd)
+            setDisable(false);
+    }
+
+    const submitRegUser = (event) => {
+        event.preventDefault();
+        
+        enteredUser = {
+            firstName : registerUserFName.current.value,
+            lastName : registerUserLName.current.value,
+            location : registerUserLocation.current.value,
+            phoneNumber : registerUserPhone.current.value,
+            email : registerUserEmail.current.value,
+            password : registerUserPassword.current.value
+        };
+        console.log(enteredUser);
+    }
+
     return (
         <>
             <Card border="" style={{ width: '28rem', float: 'left', margin: '35px' }}>
                 <Card.Header className="text-center"><span>Register User</span></Card.Header>
                 <Card.Body>
-                    <Form>
+                    <Form onSubmit={submitRegUser}>
                         <Form.Group className="mb-3" controlId="registerUserFName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="Text" placeholder="Enter First Name" />
+                            <Form.Control type="Text" placeholder="Enter First Name" ref={registerUserFName}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="registerUserLName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="Text" placeholder="Enter Last Name" />
+                            <Form.Control type="Text" placeholder="Enter Last Name" ref={registerUserLName}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="registerUserLocation">
                             <Form.Label>Location</Form.Label>
-                            <Form.Control type="Text" placeholder="Enter Location" />
+                            <Form.Control type="Text" placeholder="Enter Location" ref={registerUserLocation}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="registerUserPhone">
                             <Form.Label>Phone Number</Form.Label>
-                            <Form.Control type="Text" placeholder="Enter Phone Number" />
+                            <Form.Control type="Text" placeholder="Enter Phone Number" ref={registerUserPhone}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="registerUserEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" placeholder="Enter email" ref={registerUserEmail}/>
                         </Form.Group>
 
                         <Form.Group className="mb-2" controlId="registerUserPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" ref={registerUserPassword}/>
                         </Form.Group>
 
                         <Form.Group className="mb-2" controlId="registerUserConfirmPassword">
                             <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="confirmPassword" placeholder="Confirm Password" />
+                            <Form.Control type="password" placeholder="Confirm Password" ref={registerUserConfirmPassword} onChange={matchPassword}/>
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" disabled={disable}>
                             Submit
                         </Button>
                     </Form>

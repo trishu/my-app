f788aba32ca6c986cc439e147191adcef8d1db2d
mohan0912 <81211542+mohan0912@users.noreply.github.com>
2021-10-13 12:28:55 +0530

    added code to handle Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,72 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+import UserService from '../services/UserService.js';
 
 const Login = () => {
+    const [userLogged, setUserLogged] = useState([]);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginError, setloginError] = useState();
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        setloginError();
+        
+        if (email.length !== 0 && password.length !== 0) {
+            const user = {
+                "email": email,
+                "password": password
+            }
+            console.log(user);
+            let data = await UserService.getUser(user);
+            if (data.length !== 0) {
+                setUserLogged(data);
+                console.log(userLogged);
+                window.sessionStorage.setItem("LoggedIn", "True");
+                data.forEach((user) => window.sessionStorage.setItem("user id", user.id))
+            }
+            else {
+                setloginError("Incorrect Email or Password. Please try again");
+            }
+        }
+        else if(email.length === 0 && password.length !== 0){
+            setloginError("Enter Email Address");
+        }
+        else if(password.length === 0 && email.length !== 0){
+            setloginError("Enter Password");
+        }
+        else {
+            setloginError("Enter Email Address and password");
+        }
+    }
+
     return (
         <>
             <Card border="" style={{ width: '28rem', float: 'right', margin: '35px' }}>
                 <Card.Header className="text-center"><span>Login</span></Card.Header>
                 <Card.Body>
-                    <Form>
+                    <div id="loginErr" style={{ color: 'red' }}>{loginError} </div>
+                    <Form onSubmit={loginUser}>
                         <Form.Group className="mb-3" controlId="loginEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} />
                         </Form.Group>
 
                         <Form.Group className="mb-2" controlId="loginPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" onChange={handlePasswordChange} />
                         </Form.Group>
-                        
+
                         <Button variant="success" type="submit">
                             Login
                         </Button>

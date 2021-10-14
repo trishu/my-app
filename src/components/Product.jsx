44c5b119c31517b4444caeec7412f9fa6c5aad5b
mohan0912 <81211542+mohan0912@users.noreply.github.com>
2021-10-14 14:15:48 +0530

    handled delete confimation dialog

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -50,12 +50,23 @@ export default Product;
 
 const ProductRow = (props, key) => {
     const [show, setShow] = useState(false);
+    const prodId = props.rowdata.id;
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const handleDelete =()=>{
+    const handleDeleteClose = () => setShow(false);
+    const handleDeleteShow = () => setShow(true);
+    const handleDelete = () => {
+        console.log(prodId);
         console.log("Product got deleted");
         setShow(false);
+    }
+    const handleViewProd = () => {
+        console.log(prodId);
+        console.log("Product got viewed");
+    }
+    const handleUpdateProd = () => {
+        console.log(prodId);
+        console.log("Product got updated");
+
     }
     return (
         <>
@@ -65,15 +76,19 @@ const ProductRow = (props, key) => {
                 <td>{props.rowdata.productManufacturer}</td>
                 <td>{props.rowdata.productPrice}</td>
                 <td>{props.rowdata.productQuantity}</td>
-                <td><Button variant="info" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }}><GrView /> View</Button><Button variant="primary" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }}><GrUpdate /> Update</Button><Button variant="danger" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }} onClick={handleShow}><RiDeleteBinLine /> Delete</Button></td>
+                <td>
+                    <Button variant="info" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }} onClick={handleViewProd}><GrView /> View</Button>
+                    <Button variant="primary" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }} onClick={handleUpdateProd}><GrUpdate /> Update</Button>
+                    <Button variant="danger" size="sm" style={{ marginRight: "10px", marginLeft: "10px" }} onClick={handleDeleteShow}><RiDeleteBinLine /> Delete</Button>
+                </td>
             </tr>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleDeleteClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Delete Confirmation</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>Are you sure you want to delete this Product</Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button variant="secondary" onClick={handleDeleteClose}>
                         Close
                     </Button>
                     <Button variant="danger" onClick={handleDelete}>

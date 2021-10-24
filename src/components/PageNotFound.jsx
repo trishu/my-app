4023161a52ae83747b90416abd2b3b27d5273697
mohan0912 <81211542+mohan0912@users.noreply.github.com>
2021-10-24 21:20:10 +0530

    PageNotFound Changes

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,10 @@
+import pageNotFound from '../images/pageNotFound.jpg'
+
 const PageNotFound = () => {
     return (<>
-    <div>404 Page Not Found</div>
+    <div>
+    <img src={pageNotFound} alt="page not found" style={{width:'75%',height:'75%',marginLeft:'15%',marginTop:'5%'}}/>
+    </div>
     </>);
 }
 
